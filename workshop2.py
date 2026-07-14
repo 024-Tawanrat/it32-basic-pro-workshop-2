f69cb81054dae2c44d4bd5ee5cb524a90ea43210
024-Tawanrat <tawanrat.sittib@mail.kmutt.ac.th>
2026-07-14 22:55:17 +0700

    workshop-2(new)

--- workshop2.py
+++ workshop2.py
@@ -1,12 +1,26 @@
-name = input()
-age = int(input())
-height = float(input())
-power = int(input())
-money = int(input())
+name = input("ชื่อ: ")
+age = int(input("อายุ: "))
+height = float(input("ส่วนสูง: "))
+power = int(input("พละกำลัง: "))
+money = int(input("เงินติดตัว: "))
 
-if (age >= 50) and (power > 5):
-    print("pass")
-elif (age <50) and (power > 7):
-    print("pass")
+if (power>=1) and (power<=10) :
+    if age>=20:
+        if (power>5) and (height>=160):
+            if power>8:
+                if (money >= 10000):
+                    print("ผ่าน!!! ตำแหน่ง:ผู้บัญชาการ")
+                else:
+                    print("ผ่าน!!! ตำแหน่ง:รองผู้บัญชาการ")
+            else:
+                print("ผ่าน!!! ตำแหน่ง:จัดหาอาวุธ")
+        elif height>=999:
+            print("ผ่าน!!! ตำแหน่ง:ผู้สังเกตการ")
+        elif money>=100000:
+            print("ผ่าน!!! ตำแหน่ง:ผู้สนับสนุนทางการเงิน")
+        else:
+            print("ไม่ผ่าน:(")
+    else:
+        print("ไม่ผ่าน:(")
 else:
-    print("nOT PASS")
+    print("พละกำลัง 1-10 เท่านั้น")
